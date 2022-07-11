250e2c59d23a2b6df931814310cdd1906c3b0649
Piotr Krasny <krasny.piotr@gmail.com>
2022-07-11 08:42:33 +0200

    Fix layout

--- src/components/organisms/Layout/Layout.tsx
+++ src/components/organisms/Layout/Layout.tsx
@@ -5,9 +5,11 @@ import { Navbar } from 'components/organisms/Navbar/Navbar';
 import CustomStyles from 'styles/CustomStyles';
 import GlobalStyles from 'styles/GlobalStyles';
 import Typography from 'styles/Typography';
+import { CommonProps } from 'types/commonProps';
 
+interface Props extends CommonProps {}
 
-export const Layout: React.FunctionComponent = ({ children }): JSX.Element => (
+export const Layout: React.FunctionComponent<Props> = ({ children }): JSX.Element => (
     <>
         <GlobalStyles />
         <Typography />
