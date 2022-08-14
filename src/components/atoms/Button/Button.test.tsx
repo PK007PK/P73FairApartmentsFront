52eefd81fa7d40f05beb165957149d9173692968
Piotr Krasny <krasny.piotr@gmail.com>
2022-08-14 17:02:19 +0200

    Add basic Button tests

---
+++ src/components/atoms/Button/Button.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+// import "@testing-library/jest-dom/extend-expect";
+import '@testing-library/jest-dom'
+import { Button } from "./Button";
+
+
+describe('Test Button component', ()=>{
+    it('Render the component', ()=>{
+        render(<Button>Submit</Button>)
+        screen.getByText('Submit')
+    })
+})
