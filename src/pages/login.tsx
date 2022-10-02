773dae35db6470ef0189314a330ee6eec84b4a29
Piotr Krasny <krasny.piotr@gmail.com>
2022-10-02 19:54:14 +0200

    Add login

---
+++ src/pages/login.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+import { Layout } from "src/components/organisms/Layout/Layout";
+import { SEO } from "src/components/atoms/SEO/SEO";
+import { SectionHero } from "src/components/molecules/SectionHero/SectionHero";
+import { HeroTextBlock } from "src/components/atoms/HeroTextBlock/HeroTextBlock";
+import { Image } from "src/components/atoms/Image/Image";
+import { ImgPlaceholder } from "src/components/molecules/ImgPlaceholder/ImgPlaceholder";
+import {
+  BootsColumn,
+  BootsContainer,
+  BootsRow,
+} from "src/components/atoms/BootsElements/BootsElements";
+import { ApartmentDetails } from "src/components/molecules/ApartmentDetails/ApartmentDetails";
+
+import { PublicApartmentEntity } from "src/types/apartment";
+import { alternateValue } from "src/utils/alternateValue";
+import { detectEnvForApiCalls } from "src/utils/detectEnvForApiCalls";
+import Register from "src/components/molecules/Registration/Registration";
+import Dashboard from "src/components/molecules/Dashboard/Dashboard";
+import Login from "src/components/molecules/Login/Login";
+
+const LoginPage = () => {
+  return (
+    <Layout>
+      <SEO title="O nas" />
+      <div> a</div>
+      <div> a</div>
+      <div> a</div>
+      <div> a</div>
+      <div> a</div>
+      <div> a</div>
+      <div> a</div>
+      <BootsContainer>
+        <Login />
+      </BootsContainer>
+    </Layout>
+  );
+};
+
+export default LoginPage;
